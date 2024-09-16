fdbc194c7c841b601d5712da6c27d3159f84f785
Wuji Dems <164370429+Wujipao@users.noreply.github.com>
2024-09-16 21:46:32 +0800

    Add files via upload

---
+++ DequeTestt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class DequeTestt
+    {
+        public static void Main(string[] args)
+        {
+            DequePinca<int> dq = new DequePinca<int>(5);
+
+            dq.AddFront(1);
+            dq.AddFront(2);
+            dq.AddFront(3);
+            dq.AddFront(4);
+            dq.AddFront(5);
+
+            dq.Display();
+
+            dq.RemoveBack();
+
+            dq.Display();
+
+
+        }
+    }
+}
